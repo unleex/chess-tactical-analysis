d9853c31636c10250770dbcf0a67be74f9b22966
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2022-07-19 15:29:29 -0500

    add move to move history when a move is made
    Can now provide correct move name (eg. Nf3) after a piece has moved to
    some square. Does not handle captures or promotion yet.
    
    The move is also added to the move history list, along with the turn
    number.

--- chess/game.py
+++ chess/game.py
@@ -50,16 +50,42 @@ class ChessGame(QWidget):
     def moveToSquare(self, newSquare: Square):
         """Move piece in selected square to passed in square. Changes
         attributes of the squares to reflect the move, and returns the
-        squares to allow the BoardView to redraw the piece."""
+        squares to allow the BoardView to redraw the piece. Also adds
+        the move to the move list"""
         if newSquare in self.possibleSquares:
+            # Add move to to the move list.
+            moveName = self.createMoveName(self.selectedSquare, newSquare)
+            self.gameInfo.moveList.addMove(moveName, isWhite=self.whiteTurn)
+
+            # Because we want to return the selectedSquare and set 
+            # self.selectedSquare to None, we give it the name prevSquare.            
             prevSquare = self.selectedSquare
-            self.selectedSquare = None
-            self.possibleSquares = []
-            self.whiteTurn = True if self.whiteTurn is False else False
             newSquare.setPiece(prevSquare.getPiece())
             prevSquare.setPiece(None)
+            self.selectedSquare = None
+            self.possibleSquares = []
+
+            self.whiteTurn = True if self.whiteTurn is False else False
             return newSquare, prevSquare
-        
+
+    def createMoveName(self, oldSquare: Square, newSquare: Square):
+        """Creates a move name (eg. Nf3) from looking at a pieces'
+        current square (oldSquare) and the square it's going to move
+        to (newSquare)"""
+        # Abbreviation for knight in moves is N, as the King takes K
+        if (pieceName := oldSquare.getPiece()[1:]) == "Knight":
+            abbr = "N"
+        elif pieceName == "Pawn":
+            abbr = ""
+        else:
+            abbr = pieceName[0]
+
+        # TODO: MOVE NAMES FOR PROMOTION
+
+        # Checks if it's a capture
+        if newSquare.hasPiece():
+            return abbr + 'x' + newSquare.getName()
+        return abbr + newSquare.getName()
 
 class GameInfo(QFrame):
 
@@ -95,14 +121,21 @@ class MoveList(QFrame):
         """Creates frame that will hold move history"""
         moveList = QFrame()
         layout = QGridLayout()
+        for i in range(20):
+            layout.setRowStretch(i, 1)
+        layout.setColumnStretch(0, 1)  # Column showing turn number should be smaller.
+        layout.setColumnStretch(1, 2)
+        layout.setColumnStretch(2, 2)
         moveList.setLayout(layout)
         return moveList, layout
 
     def addMove(self, move: str, isWhite=True):
         """Adds move to the move history"""
+        turnLabel = QLabel(str(self.row + 1))
         label = QLabel(move)
         if isWhite:
-            self.moveListLayout.addWidget(label, self.row, 0)
-        else:
+            self.moveListLayout.addWidget(turnLabel, self.row, 0)
             self.moveListLayout.addWidget(label, self.row, 1)
+        else:
+            self.moveListLayout.addWidget(label, self.row, 2)
             self.row += 1
